7f9bafcf9d092ad1257f0d0f1b4dce7e97b3d6b1
asafigan <asafigan@gmail.com>
2016-03-26 04:23:43 +0000

    started setting up users. need to add session functionality.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    User.find_by_id(session[:user_id])
+  end
+
+  def redirect_non_user(to: '/')
+    redirect_to(to) unless current_user
+  end
+
+  def redirect_non_admin(to: '/')
+    redirect_to(to) unless current_user.is_admin?
+  end
+
+  def redirect_non_player(to: '/')
+    redirect_to(to) unless current_user.is_player?
+  end
 end
