9ec7d2844378f26f30255bcb65fdcf74569ad043
asafigan <asafigan@gmail.com>
2016-03-28 15:42:16 -0400

    fixing player controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,32 +2,24 @@ class PlayersController < ApplicationController
   before_action :set_player, only: [:show, :edit, :update, :destroy]
   before_action :redirect_non_user
   before_action :redirect_non_admin, only: [:index, :new, :create, :destroy]
-
-  # GET /users
-  # GET /users.json
+  
   def index
     @players = User.find_all_by_role(:player)
   end
-
-  # GET /users/1
-  # GET /users/1.json
+  
   def show
   end
 
-  # GET /users/new
   def new
     @player = User.new
-    @player.role= :player
   end
 
-  # GET /player/1/edit
   def edit
   end
 
-  # POST /player
-  # POST /player.json
   def create
     @player = User.new(user_params)
+    @player.role = :player
 
     respond_to do |format|
       if @player.save
@@ -40,8 +32,6 @@ class PlayersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
       if @player.update(player_params)
@@ -54,18 +44,16 @@ class PlayersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     if @player
       @user.destroy
       respond_to do |format|
-        format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+        format.html { redirect_to players_url, notice: 'Player was successfully destroyed.' }
         format.json { head :no_content }
       end
     else
       respond_to do |format|
-        format.html { redirect_to users_url, notice: 'User is not a player.' }
+        format.html { redirect_to players_url, notice: 'User is not a player.' }
         format.json { head :no_content }
       end
     end
@@ -74,8 +62,8 @@ class PlayersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_player
-      player = User.find(params[:id])
-      @player = player if user.is_player?
+      user = User.find(params[:id])
+      @player = user if user.is_player?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
