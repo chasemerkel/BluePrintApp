5fe64aa694db4bfcdea4df701ddd1fd448a3181e
asafigan <asafigan@gmail.com>
2016-03-28 14:40:31 -0400

    added error message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,8 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_url
     else
-      redirect_to login_url
+      @errors = ['User Name or Password was incorrect']
+      format.html { render :new }
     end
   end
 
