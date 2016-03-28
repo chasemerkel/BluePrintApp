7605c7381fbfe9c43ec20b705556761c8e118534
asafigan <asafigan@gmail.com>
2016-03-28 15:23:56 -0400

    fixing logout route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   
   get 'login' => 'sessions#new', as: :login
   post 'login' => 'sessions#create'
-  post 'logout' => 'sessions#destroy', as: :logout
+  delete 'logout' => 'sessions#destroy', as: :logout
   
   resources :players
   
