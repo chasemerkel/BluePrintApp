53a1ef4373bf161eb8c5a238714e9a3483cb0bd0
asafigan <asafigan@gmail.com>
2016-03-28 14:58:08 -0400

    created pages controller. added home page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,1 +1,22 @@
+class PagesController < ApplicationController
+  
+  def home
+    non_user_home unless current_user
+    player_home if current_user.is_player?
+    admin_home if current_user.is_admin?
+  end
+  
+private
 
+  def admin_home
+    format.html{render :admin_home}
+  end
+  
+  def player_home
+    format.html{render :player_home}
+  end
+  
+  def non_user_home
+    redirect_to login_url
+  end
+end
