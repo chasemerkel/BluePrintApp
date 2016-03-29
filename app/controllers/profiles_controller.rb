574eb71744af26edf535c5a136e43a2783aa95b4
asafigan <asafigan@gmail.com>
2016-03-29 17:59:26 -0400

    added authentication to update_password

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,20 +8,22 @@ class ProfilesController < ApplicationController
   end
 
   def update_password
-    respond_to do |format|
-      if current_user.update(profile_params)
-        format.html { redirect_to @user, notice: 'Your profile was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+    if current_user.authenticate(password_params[:old_password])
+      respond_to do |format|
+        if current_user.update(password: password_params[:new_password], confirm_password: password_params[:confirm_password])
+          format.html { redirect_to @user, notice: 'Your password was successfully updated.' }
+          format.json { render :show, status: :ok, location: @user }
+        else
+          format.html { render :change_password }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
 
   private
     # Never trust parameters from the scary internet, only allow the white list through.
-    def update_password_params
+    def password_params
       params.require(:profile).permit(:new_password, :confirm_password, :old_password)
     end
 end
